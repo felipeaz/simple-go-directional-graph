a33db58cc88f1c5052df29429b17d2ebf1bcb453
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-01-27 19:36:50 -0300

    Randomizing a way home

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 )
 
 const (
 	errNodeIsNotEmpty     = "graph %s is already pointing to another graph on its %s"
 	errDirectionNotMapped = "direction %s is not mapped"
+
+	nowhereToGo       = "Ops, looks like there's nowhere to go from %s. Trying another way\n"
+	endOfTheLine      = "End of the line, nowhere to go from %s. Going back to %s"
+	navigatingMessage = "Going %s. Heading to %s\n"
+	arrivedMessage    = "Got home after walking %d miles"
+
+	gasolineStationDest = "Gasoline Station"
+	policeStationDest   = "Police Station"
+	supermarketDest     = "Supermarket"
+	shoppingDest        = "Shopping"
+	homeDest            = "Home"
+
+	goLeft  = 0
+	goRight = 1
+
+	failuresCount = 0
 )
 
 type nodeDirection string
@@ -42,16 +60,17 @@ func (g *BidirectionalGraph) LinkNode(node *BidirectionalGraph, direction nodeDi
 
 func NewGraph(name string, weight int) *BidirectionalGraph {
 	return &BidirectionalGraph{
+		Name:   name,
 		Weight: weight,
 	}
 }
 
 func main() {
-	gasolineStation := NewGraph("Gasoline Station", 0)
-	shopping := NewGraph("Shopping", 5)
-	superMarket := NewGraph("Supermarket", 3)
-	policeStation := NewGraph("Police Station", 7)
-	home := NewGraph("Home", 9)
+	gasolineStation := NewGraph(gasolineStationDest, 0)
+	shopping := NewGraph(shoppingDest, 5)
+	superMarket := NewGraph(supermarketDest, 3)
+	policeStation := NewGraph(policeStationDest, 7)
+	home := NewGraph(homeDest, 9)
 
 	/* 		    (Gasoline Station)
 				/				\
@@ -66,4 +85,61 @@ func main() {
 	gasolineStation.LinkNode(superMarket, right)
 	superMarket.LinkNode(shopping, right)
 	superMarket.LinkNode(home, left)
+
+	RandomWayHome(gasolineStation)
+}
+
+func RandomWayHome(node *BidirectionalGraph) {
+	var ok bool
+	rand.Seed(time.Now().UnixNano())
+
+	totalDistance := node.Weight
+	startingPoint := node
+	currentPoint := node
+	for currentPoint.Name != homeDest {
+		randomNumber := rand.Intn(2)
+		switch randomNumber {
+		case goLeft:
+			currentPoint, ok = navigate(startingPoint, currentPoint, left)
+			if ok {
+				totalDistance += currentPoint.Weight
+			}
+		case goRight:
+			currentPoint, ok = navigate(startingPoint, currentPoint, right)
+			if ok {
+				totalDistance += currentPoint.Weight
+			}
+		}
+	}
+	log.Printf(arrivedMessage, totalDistance)
+}
+
+func navigate(startPoint, currentPoint *BidirectionalGraph, direction nodeDirection) (*BidirectionalGraph, bool) {
+	if currentPoint.LeftNode == nil && currentPoint.RightNode == nil {
+		log.Printf(endOfTheLine, currentPoint.Name, startPoint.Name)
+		return startPoint, false
+	}
+	nextPoint := currentPoint
+	switch direction {
+	case right:
+		if currentPoint.RightNode == nil {
+			log.Printf(nowhereToGo, direction)
+			return navigate(startPoint, currentPoint, left)
+		}
+		log.Printf(navigatingMessage, direction, currentPoint.RightNode.Name)
+		startPoint = currentPoint
+		nextPoint = currentPoint.RightNode
+	case left:
+		if currentPoint.LeftNode == nil {
+			log.Printf(nowhereToGo, direction)
+			return navigate(startPoint, currentPoint, right)
+		}
+		log.Printf(navigatingMessage, direction, currentPoint.RightNode.Name)
+		startPoint = currentPoint
+		nextPoint = currentPoint.LeftNode
+	default:
+		log.Fatalf(errDirectionNotMapped, direction)
+	}
+
+	return nextPoint, true
 }
